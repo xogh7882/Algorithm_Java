0bdb482becd6e4ecd4bc34821449ac1f5a24c253
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-01-28 09:32:50 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 19364 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,50 @@
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] nums;
+	static boolean[] visited;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[M];
+		visited = new boolean[N];
+		
+		StringBuilder sb = new StringBuilder();
+		perm(0,0,sb);
+		System.out.println(sb.toString());
+		
+		
+	}
+
+	private static void perm(int depth, int start, StringBuilder sb) {
+		if(depth == M) {
+			for(int i=0;i<M;i++) {
+				sb.append(nums[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=start;i<N;i++) {
+			//if(visited[i]) continue;
+			visited[i] = true;
+			nums[depth] = i+1;
+			perm(depth+1,i,sb);
+			nums[depth] = 0;
+			visited[i] = false;
+		}
+		
+	}
+
+}
