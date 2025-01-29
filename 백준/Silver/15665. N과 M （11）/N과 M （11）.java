a65341ab2e6b7ef304cf47fe3072e9c89338a9a4
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-01-29 16:14:38 +0900

    [Silver II] Title: N과 M (11), Time: 396 ms, Memory: 84728 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	static int[] num;
+	static int[] select;
+	static boolean[] visited;
+	static StringBuilder sb;
+	static int flag = 0;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		num = new int[N];
+		select = new int[M];
+		visited = new boolean[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		sb = new StringBuilder();
+		
+		perm(0);
+		
+		System.out.println(sb.toString());
+
+	}
+
+	private static void perm(int cnt) {
+		if(cnt == M) {
+			for(int i=0;i<M;i++) {
+				sb.append(select[i] + " ");
+			}
+			sb.append("\n");
+			return;
+			
+		}
+		
+		int prev = -1;
+		for(int i=0;i<N;i++) {	
+			if(num[i] != prev) {
+				//visited[i] = true;
+				select[cnt] = num[i];
+				prev = num[i];
+				perm(cnt+1);
+				//visited[i] = false;
+			}
+		}
+	}
+
+}
