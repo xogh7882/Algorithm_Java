3ecefe4ef40cf3330415e8a17691af18c31633a9
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-02-26 23:43:06 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1216 ms, Memory: 343880 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M,K;
+	static int map[][];
+	static int dr[] = {1,-1,0,0};
+	static int dc[] = {0,0,1,-1};
+	
+	static int count[][][];    // 값 계산
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		map = new int[N][M];
+		for(int i=0;i<N;i++) {
+			String str = br.readLine();
+			for(int j=0;j<M;j++) {
+				if(str.charAt(j) =='0') map[i][j] = 0;
+				else map[i][j] = 1;
+			}
+		}
+		
+		count = new int[K+1][N][M];
+		
+		int result = BFS();
+		
+		System.out.println(result);
+	}
+
+
+	private static int BFS() throws Exception{
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] {K,0,0});
+		count[K][0][0] = 1;
+		
+		while(!queue.isEmpty()) {
+            
+			int temp[] = queue.poll();
+			int k = temp[0];
+			int r = temp[1];
+			int c = temp[2];
+			
+			if(r==N-1 && c==M-1) return count[k][r][c];
+			
+			for(int i=0;i<4;i++){
+				int nr = r + dr[i];
+				int nc = c + dc[i];
+				
+				
+				if(check(nr,nc)==false) continue;
+				
+				if(map[nr][nc]==1) {
+					
+					if(k==0) continue;
+					else {
+						if(count[k-1][nr][nc] == 0) {
+							count[k-1][nr][nc] = count[k][r][c] + 1;
+							queue.offer(new int[] {k-1,nr,nc});
+						
+						}
+					}
+				}
+				else {
+					if(count[k][nr][nc] == 0) {
+						count[k][nr][nc] = count[k][r][c] + 1;
+						queue.offer(new int[] {k,nr,nc});
+						
+					}
+				}
+			}
+		}
+		
+		return -1;
+	}
+
+	private static boolean check(int r, int c) {
+		return r>=0 && r<N && c>=0 && c<M;
+	}
+
+}
