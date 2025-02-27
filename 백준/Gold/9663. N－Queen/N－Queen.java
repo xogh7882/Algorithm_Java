d515f221ddacc787e6bed0776605a3baaccecfe2
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-02-27 22:39:39 +0900

    [Gold IV] Title: N-Queen, Time: 2588 ms, Memory: 12128 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,61 +1,43 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
-	static int count = 0;
-	static int[][] map;
-	static boolean[] visited;
-	static int[] q;
+	static int result = 0;
+	static boolean column[];
+	static boolean slash[];
+	static boolean bslash[];
 	
-	// 대각선만 체크하면 되는데
-	// 행 열 끼리 빼면 값이 똑같네
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		map = new int[N][N];
-		q = new int[N];
-		visited = new boolean[N];
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		column = new boolean[N+1];  // 1부터 N까지
+		slash = new boolean[2*N+1]; // i+j
+		bslash = new boolean[2*N];  // (i-j)+N
 		
-		queen(0);
-		System.out.println(count);
+		queen(1);
+		
+		System.out.println(result);
 	}
 
-	private static void queen(int cnt) {
-		if(cnt == N) {
-			count++;
+	private static void queen(int row) {
+		if(row > N) {
+			result++;
 			return;
 		}
-		
-		for(int i=0;i<N;i++) {
-			if(visited[i] == false && checkqueen(cnt,i)) {  // 지금까지 들어온 queen 이랑 비교
-				visited[i] = true;
-				q[cnt] = i;
-				queen(cnt+1);
-				q[cnt] = 0;
-				visited[i] = false;
-			}
+		for(int c=1;c<=N;c++) {
+			if(!check(row, c)) continue;  // 세로, 대각, 반대대각 3개 모두 놓을 수 있니?
+			column[c] = slash[row+c] = bslash[row-c+N] = true;
+			queen(row+1);
+			column[c] = slash[row+c] = bslash[row-c+N] = false;
 		}
 		
 	}
 
-	private static boolean checkqueen(int cnt, int i) {  // 새로 들어오는 queen 이랑 안만나면 true
-		if(cnt==0) return true;
-		
-		int r1 = cnt;
-		int c1 = i;
-		
-		for(int x=0;x<cnt;x++) {
-			int r2 = x;
-			int c2 = q[x];
-			
-			if(Math.abs(r1-r2) == Math.abs(c1-c2)) return false;
-			else continue;
-		}
-		
-		return true;
+	private static boolean check(int row, int c) {
+		return column[c] == false && slash[row+c]==false && bslash[row-c+N]==false;
 	}
 
 }
