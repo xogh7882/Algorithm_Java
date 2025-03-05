f29a0778f5a0cc8646a2d88cce2a4e2263819def
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-03-05 21:57:53 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 160 ms, Memory: 47828 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -6,64 +6,98 @@ import java.util.StringTokenizer;
 
 public class Main {
 	static int N,K;
+	static boolean visited[];
 	static int result = Integer.MAX_VALUE;
-	static int time = 0;
-	static int count = 0;
+	static int num = 0;
 	
-	public static void main(String[] args) throws Exception{
+	static class Tree{
+		int num;
+		Tree parent;
+		Tree left;
+		Tree mid;
+		Tree right;
+		int level;
+		
+		public Tree(int num, Tree parent, Tree left, Tree mid, Tree right, int level) {
+			super();
+			this.num = num;
+			this.parent = parent;
+			this.left = left;
+			this.mid = mid;
+			this.right = right;
+			this.level = level;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		BFS(N);
-		
-		System.out.println(result);
-		System.out.println(count);
-		
+		if(N>K) {    // 갈 수 있는 방법이 -1 해서 가는 것 뿐
+			System.out.println(N-K);
+			System.out.println(1);
+		}
+		else if(N==K) {  // 이미 끝남
+			System.out.println(0);
+			System.out.println(1);
+		}
+		else {
+			Tree root = new Tree(N,null,null,null,null,0);
+			visited = new boolean[K*2+1];
+			visited[root.num] = true;
+			
+			BFS(root);  // 트리 만들기
+			
+			System.out.println(result);
+			System.out.println(num);
+		}
 	}
 
-	private static void BFS(int start) {
-		boolean visited[] = new boolean[Math.max(N, K)*2+1];
-		
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.offer(start);
-		int size = queue.size();
+
+	private static int BFS(Tree root) {
+		Queue<Tree> queue = new ArrayDeque<>();
+		queue.offer(root);
 		
 		while(!queue.isEmpty()) {
-			if(size==0) {
-				size = queue.size();
-				time++;
-			}
+			Tree temp = queue.poll();
+			visited[temp.num] = true;
 			
-			int num= queue.poll();
-			visited[num] = true;
-			size--;
 			
-			if(num == K) {
-				if(time < result) {
-					result = time;
-					count = 1;
+			if(temp.num==K) {
+				if(temp.level < result) {
+					result = temp.level;
+					num = 1;
 				}
-				else if(time == result) {
-					count++;
+				else if(temp.level == result) {
+					num++;
 				}
+				continue;
 			}
 			
-			if(num*2>=0 && num*2 <= K*2 && visited[num*2]==false) {
-				queue.offer(num*2);
+			
+			if(temp.num-1 >= 0 && visited[temp.num-1] == false && temp.level <= result) {
+				temp.left = new Tree(temp.num-1,temp,null,null,null,temp.level+1);
+				queue.offer(temp.left);
 			}
 			
-			if(num+1 >= 0 && num+1 <= K && visited[num+1] == false) {
-				queue.offer(num+1);
+			
+			if( temp.num+1 <=K && visited[temp.num+1] == false && temp.level <= result) {
+				temp.right = new Tree(temp.num+1,temp,null,null,null,temp.level+1);
+				queue.offer(temp.right);
 			}
 			
-			if(num-1>=0 && visited[num-1] == false) {
-				queue.offer(num-1);
+			
+			if( temp.num*2 <= K*2 && visited[temp.num*2] == false && temp.level <= result) {
+				temp.mid = new Tree(temp.num*2,temp,null,null,null,temp.level+1);
+				queue.offer(temp.mid);
 			}
 		}
-			
 		
+		return -1;
 	}
 
 }
