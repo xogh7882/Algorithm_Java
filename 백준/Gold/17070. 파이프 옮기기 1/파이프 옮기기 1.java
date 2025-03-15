87a1af40edc77cc483bf5cb20e5a67585c53346a
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-03-15 15:56:15 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 244 ms, Memory: 13468 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,20 +1,18 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int N;
 	static int map[][];
+	static boolean visited[][];
 	static int result = 0;
 	
 	static int dr[] = {0,1,1};
 	static int dc[] = {1,1,0};
-
 	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
@@ -26,77 +24,54 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		BFS();
+		
+		visited = new boolean[N][N];
+		
+		DFS(0, 0, 1);
 		
 		System.out.println(result);
 		
+
 	}
-	
+
 	// 가로는 01
 	// 세로는 12
 	// 대각은 012
 	// 가로->0  , 대각->1,  세로->2
 	
 	// k = 0 ( 현재가로 ) / k=1(현재대각) / k=2(현재세로)
-	private static void BFS() {
-		Queue<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[] {0,0,0,0,1});
+	
+	private static void DFS(int k, int r, int c) {
+		if(r == N-1 && c == N-1) {
+			result++;
+			return;
+		}
 		
-		while(!queue.isEmpty()) {
-			int temp[] = queue.poll();
-			int k = temp[0];
-			int prevR = temp[1];
-			int prevC = temp[2];
-			int nowR = temp[3];
-			int nowC = temp[4];
-			if(nowR == N-1 && nowC == N-1) {
-				result++;
-				continue;
-			}
-			if(k==0) {  // 현재 가로
-				for(int i=0;i<2;i++) {
-					int nprevR = nowR;
-					int nprevC = nowC;
-					int nnowR = nowR + dr[i];
-					int nnowC = nowC + dc[i];
-					if(check(nnowR, nnowC) == true && map[nnowR][nnowC]==0) {
-						if(i==0) queue.add(new int[] {0,nprevR, nprevC, nnowR, nnowC});
-						else if(i==1) {
-							if(map[nnowR-1][nnowC] == 0 && map[nnowR][nnowC-1] == 0)
-								queue.add(new int[] {1,nprevR, nprevC, nnowR, nnowC});
-						}
-					}
+		int start;
+		int end;
+		if(k==0) {
+			start = 0;
+			end = 2;
+		}
+		else if(k==1) {
+			start=0;
+			end=3;
+		}
+		else {
+			start=1;
+			end=3;
+		}
+		
+		for(int i=start;i<end;i++) {
+			int nr = r + dr[i];
+			int nc = c + dc[i];
+			if(check(nr,nc) && map[nr][nc] == 0) {
+				if(i==0 || i==2) {
+					DFS(i,nr,nc);
 				}
-			}
-			else if(k==1) {  // 현재 대각
-				for(int i=0;i<3;i++) {
-					int nprevR = nowR;
-					int nprevC = nowC;
-					int nnowR = nowR + dr[i];
-					int nnowC = nowC + dc[i];
-					if(check(nnowR, nnowC) == true && map[nnowR][nnowC]==0) {
-						if(i==0) queue.add(new int[] {0,nprevR, nprevC, nnowR, nnowC});
-						else if(i==1) {
-							if(map[nnowR-1][nnowC] == 0 && map[nnowR][nnowC-1] == 0)
-								queue.add(new int[] {1,nprevR, nprevC, nnowR, nnowC});
-						}
-						else if(i==2) queue.add(new int[] {2,nprevR, nprevC, nnowR, nnowC});
-					}
-				}
-			}
-			else if(k==2) {
-				for(int i=1;i<3;i++) {
-					int nprevR = nowR;
-					int nprevC = nowC;
-					int nnowR = nowR + dr[i];
-					int nnowC = nowC + dc[i];
-					if(check(nnowR, nnowC) == true && map[nnowR][nnowC]==0) {
-						if(i==1) {
-							if(map[nnowR-1][nnowC] == 0 && map[nnowR][nnowC-1] == 0)
-								queue.add(new int[] {1,nprevR, nprevC, nnowR, nnowC});
-						}
-						else if(i==2) queue.add(new int[] {2,nprevR, nprevC, nnowR, nnowC});
+				else{
+					if(map[nr-1][nc] == 0 && map[nr][nc-1] == 0) {
+						DFS(i,nr,nc);
 					}
 				}
 			}
@@ -108,12 +83,5 @@ public class Main {
 		return r>=0 && r<N && c>=0 && c<N;
 	}
 
-	private static void printMap() {
-		System.out.println("===========PrintMap================");
-		for(int i=0;i<N;i++) {
-			System.out.println(Arrays.toString(map[i]));
-		}
-		System.out.println();
-	}
-
+	
 }
