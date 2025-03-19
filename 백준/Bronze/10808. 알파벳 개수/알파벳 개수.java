97b730280e3efaf7d78e8fa86efcc182157ff5cb
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-03-19 21:51:34 +0900

    알파벳 개수.java 업데이트

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		int map[] = new int[26];
 		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String str = br.readLine();
 		for(int i=0;i<str.length();i++) {
