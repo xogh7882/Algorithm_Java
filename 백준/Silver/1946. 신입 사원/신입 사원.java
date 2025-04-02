a82f46851232e154bf19b9c6b4ef83c5f94335d3
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-04-02 10:35:04 +0900

    [Silver I] Title: 신입 사원, Time: 1472 ms, Memory: 300136 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.java
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -1,9 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
 	static int T;
 	static int N;
 	static class score implements Comparable<score>{
@@ -34,23 +38,31 @@ public class Main {
 			result = 0;
 			N = Integer.parseInt(br.readLine());
 			
-			PriorityQueue<score> queue = new PriorityQueue<>();
+			List<score> list = new ArrayList<>();
+			
 			for(int i=1;i<=N;i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				queue.offer(new score(a,b));
+				list.add(new score(a,b));
 			}
 			
-			int scoreBMin = N+1;
-			while(!queue.isEmpty()) {
-				score temp = queue.poll();
-				if(scoreBMin == N+1 || temp.scoreB < scoreBMin) {
-					scoreBMin = temp.scoreB;
+			Collections.sort(list);
+			
+			int min = -1;
+			
+			for(int i=0;i<N;i++) {
+				score temp = list.get(i);
+				if(i==0) {
+					min = temp.scoreB;
 					result++;
 				}
-
-//				System.out.println("(" + temp.scoreA + "," + temp.scoreB + ")");
+				else {
+					if(temp.scoreB < min) {
+						min = temp.scoreB;
+						result++;
+					}
+				}
 			}
 			
 			sb.append(result).append("\n");
