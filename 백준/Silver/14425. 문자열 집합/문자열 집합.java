b07ec028d9f8587a96e77b6f6e0e2af363e4380e
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-04-06 18:15:59 +0900

    [Silver IV] Title: 문자열 집합, Time: 2300 ms, Memory: 872436 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	static int N,M;
+	
+	static class TriNode{
+		TriNode[] children;
+		boolean isNumber;
+		
+		public TriNode() {
+			this.children = new TriNode[26];
+			this.isNumber = false;
+		}
+	}
+	
+	static TriNode root = new TriNode();
+	static int result = 0;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		for(int i=0;i<N;i++) {
+			String str = br.readLine();
+			Insert(str);
+		}
+		
+		for(int i=0;i<M;i++) {
+			String str = br.readLine();
+			if(find(str)==true) result++;
+		}
+		
+		System.out.println(result);
+	}
+
+
+	private static void Insert(String str) {
+		TriNode cur = root;
+		
+		for(int i=0;i<str.length();i++) {
+			int idx = str.charAt(i) - 'a';
+			
+			if(cur.children[idx] == null) {
+				cur.children[idx] = new TriNode();
+			}
+			cur = cur.children[idx];
+		}
+		
+		cur.isNumber = true;
+	}
+
+	
+	private static boolean find(String str) {
+		TriNode cur = root;
+		
+		for(int i=0;i<str.length();i++) {
+			int idx = str.charAt(i)-'a';
+			
+			if(cur.children[idx] == null) return false;
+			
+			cur = cur.children[idx];
+		}
+		if(cur.isNumber==true) return true;
+		else return false;
+		
+	}
+}
