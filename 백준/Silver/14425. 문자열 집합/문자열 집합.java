2bfee8ee5b2c39df591c2429c2b8967b99f35aba
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-04-06 18:22:44 +0900

    [Silver IV] Title: 문자열 집합, Time: 288 ms, Memory: 38944 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,24 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
+// HashSet
 
 public class Main {
 	static int N,M;
-	
-	static class TriNode{
-		TriNode[] children;
-		boolean isNumber;
-		
-		public TriNode() {
-			this.children = new TriNode[26];
-			this.isNumber = false;
-		}
-	}
-	
-	static TriNode root = new TriNode();
 	static int result = 0;
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,48 +15,18 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
+		HashSet<String> set = new HashSet<>();
+		
 		for(int i=0;i<N;i++) {
 			String str = br.readLine();
-			Insert(str);
+			set.add(str);
 		}
 		
 		for(int i=0;i<M;i++) {
 			String str = br.readLine();
-			if(find(str)==true) result++;
+			if(set.contains(str)==true) result++;
 		}
-		
 		System.out.println(result);
 	}
 
-
-	private static void Insert(String str) {
-		TriNode cur = root;
-		
-		for(int i=0;i<str.length();i++) {
-			int idx = str.charAt(i) - 'a';
-			
-			if(cur.children[idx] == null) {
-				cur.children[idx] = new TriNode();
-			}
-			cur = cur.children[idx];
-		}
-		
-		cur.isNumber = true;
-	}
-
-	
-	private static boolean find(String str) {
-		TriNode cur = root;
-		
-		for(int i=0;i<str.length();i++) {
-			int idx = str.charAt(i)-'a';
-			
-			if(cur.children[idx] == null) return false;
-			
-			cur = cur.children[idx];
-		}
-		if(cur.isNumber==true) return true;
-		else return false;
-		
-	}
 }
