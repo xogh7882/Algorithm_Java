9c34be28ba31031b359525b4569e41736e50db90
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-04-19 14:22:28 +0900

    [Bronze I] Title: 약수, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		int arr[] = new int[N];
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[0] * arr[N-1]);
+
+	}
+
+}
