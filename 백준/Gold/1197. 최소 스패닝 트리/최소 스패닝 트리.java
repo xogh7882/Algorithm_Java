f32c1597b295f781f7ed331cd2eaf4aade3bcceb
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-04-22 16:41:24 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 392 ms, Memory: 47656 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,85 +4,90 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int V,E;
-	static long min;
-	static class Edge implements Comparable<Edge>{
-		int s;  // start
-		int e;   // end
-		int w;  // weight
-		public Edge(int s, int e, int w) {
+	static int N,E;
+	
+	static class Node implements Comparable<Node>{
+		int s;
+		int e;
+		int w;
+		
+		public Node(int s, int e, int w) {
 			super();
 			this.s = s;
 			this.e = e;
 			this.w = w;
 		}
+
 		@Override
-		public int compareTo(Edge o) {
+		public int compareTo(Node o) {
 			return Integer.compare(this.w, o.w);
 		}
-		
 	}
 	
-	static PriorityQueue<Edge> points;   // w가 작은 것부터 나오도록 쁘리아라라큐 사용
-	static int[] p;  // parent ( 최종 부모 )
-	static int[] r;  // rank  ( 나랑 몇개가 연결되어있게? )
+	static PriorityQueue<Node> queue = new PriorityQueue<>();
+	static int p[];
+	static int r[];
+	static long result = 0L;
 	
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		points = new PriorityQueue<>();
 		
+		N = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
 		
 		for(int i=0;i<E;i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			points.offer(new Edge(s,e,w));
+			
+			queue.offer(new Node(s,e,w));
 		}
 		
-		makeSet();
-		int cnt = 0;
-		min = 0L;
-		while(cnt != V-1) {
-			Edge edge = points.poll();
-			if(union(edge.s, edge.e)) {
-				cnt++;
-				min+=edge.w;
+		makeset();
+		
+		while(!queue.isEmpty()) {
+			Node cur = queue.poll();
+			if(union(cur.s, cur.e) == true) {
+				result += cur.w;
 			}
 		}
-		System.out.println(min);
+		
+		System.out.println(result);
+		
 	}
 
 	private static boolean union(int x, int y) {
 		x = find(x);
 		y = find(y);
-		if(x==y) return false;  // 최종 부모가 같다 = 이미 연결되어있다.
-		if(r[x] < r[y]) {    // rank가 큰 쪽으로 붙는다.
+		
+		if(x==y) return false;
+		
+		if(r[x] < r[y]) {  
 			r[y] += r[x];
 			p[x] = y;
 		}else {
 			r[x] += r[y];
 			p[y] = x;
 		}
+		
 		return true;
 	}
 
-	private static int find(int x) {   // 최종 부모까지 재귀로 찾기
-		if(x ==p[x]) return p[x];
+	private static int find(int x) {
+		if(x==p[x]) return p[x];
 		else return find(p[x]);
 	}
 
-	private static void makeSet() {
-		p = new int[V+1];
-		r = new int[V+1];
-		for (int i = 0; i < V+1; i++) {
-			p[i] = i;  // 각각 분리되어 있어서 자기가 최종 부모
-			r[i] = 1;  // 처음에는 각각 다 떨어져있어서 그룹당 1개
+	private static void makeset() {
+		p = new int[N+1];
+		r = new int[N+1];
+		for(int i=0;i<=N;i++) {
+			p[i] = i;
+			r[i] = 1;
 		}
+		
 	}
 
 }
