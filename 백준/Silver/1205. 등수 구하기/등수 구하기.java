b35ef41ba92ce94d0c267a28a9b1e830850961df
Taeho Lee <109503919+Bluestar77@users.noreply.github.com>
2025-04-28 16:22:27 +0900

    [Silver IV] Title: 등수 구하기, Time: 76 ms, Memory: 11976 KB -BaekjoonHub

---
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, P;
+	static long num;
+	
+	static long total[];
+	static int result = -1;
+	
+	static List<Long> list = new ArrayList<>();
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		num = Long.parseLong(st.nextToken());
+		P = Integer.parseInt(st.nextToken());
+		
+		int temp = Math.max(P, N);
+		
+		total = new long[temp];
+		
+		if(N==0) {
+			System.out.println(1);
+			System.exit(0);
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			list.add(Long.parseLong(st.nextToken()));
+		}
+		
+		
+		Collections.sort(list);
+		Collections.reverse(list);
+		
+		for(int i=0;i<list.size();i++) {
+			total[i] = list.get(i);
+		}
+		
+//		System.out.println(Arrays.toString(total));
+		
+		for(int i=0;i<temp;i++) {
+			if(num==0 && total[i] <= num) {
+				result = i+1;
+				System.out.println(result);
+				System.exit(0);
+				break;
+			}
+			
+			
+			if(total[i] <num) {
+				result = i+1;
+				break;
+			}
+		}
+		
+		for(int i=result-1;i>=0;i--) {
+			if(num == total[i]) {
+				result--;
+			}
+		}
+		
+		if(result > P) result = -1;
+		System.out.println(result);
+		
+	}
+
+}
