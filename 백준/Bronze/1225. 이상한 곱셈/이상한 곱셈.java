22f04316cf062a4f57e8151538ab1385cfd44729
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2025-05-02 20:52:50 +0900

    [Bronze II] Title: 이상한 곱셈, Time: 216 ms, Memory: 11824 KB -BaekjoonHub

---
+++ 백준/Bronze/1225. 이상한 곱셈/이상한 곱셈.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String A = st.nextToken();
+		String B = st.nextToken();
+		
+		long result = 0L;
+		
+		for(int i=0;i<A.length();i++) {
+			for(int j=0;j<B.length();j++) {
+				result += (A.charAt(i)-'0') * (B.charAt(j)-'0');
+			}
+		}
+		
+		System.out.println(result);
+		
+	}
+}
