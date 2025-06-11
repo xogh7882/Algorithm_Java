f214880aeebc7c58ff899b6e5ac248a5cce1b0c8
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2025-06-11 14:35:54 +0900

    [Silver III] Title: 프린터 큐, Time: 72 ms, Memory: 11952 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.java
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -3,7 +3,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 	static int T,N,M;
 	
 	public static void main(String[] args) throws Exception {
