75ff39070367f050e6363bc97279cc233422e4a5
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2025-07-11 10:01:39 +0900

    [Gold V] Title: 배열 돌리기 1, Time: 1436 ms, Memory: 136700 KB -BaekjoonHub

--- 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Gold/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -3,78 +3,76 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M,R;
-	static int[][] arr;
-	static int dr[] = {1,0,-1,0};
-	static int dc[] = {0,1,0,-1};
-	static int nowR,nowC, nowR2, nowC2;
-	static StringBuilder sb;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(
-				new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		arr = new int[N][M];
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<M;j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		int k = Math.min(N, M);
-		for(int i=0;i<R;i++) {  //R번 배열 돌리기
-			nowR = 0;
-			nowC = 0;
-			nowR2 = nowR;
-			nowC2 = nowC;
-			for(int j=0;j<k/2;j++) {
-				int temp = arr[nowR][nowC];
-				for(int x=0;x<M-1-2*j;x++) {
-					arr[nowR][nowC] = arr[nowR][nowC+1];
-					nowC++;
-				}
-				
-				for(int x=0;x<N-1-2*j;x++) {
-					arr[nowR][nowC] = arr[nowR+1][nowC];
-					nowR++;
-				}
-				
-				for(int x=0;x<M-1-2*j;x++) {
-					arr[nowR][nowC] = arr[nowR][nowC-1];
-					nowC--;
-				}
-				
-				for(int x=0;x<N-1-2*j;x++) {
-					arr[nowR][nowC] = arr[nowR-1][nowC];
-					nowR--;
-				}
-				
-				arr[nowR2+1][nowC2] = temp;
-				nowR = nowR2 + 1;
-				nowC = nowC2 + 1;
-				nowR2 = nowR;
-				nowC2 = nowC;
-			}
-			
-		}
-		
-		print_arr();
-				
+    static int N,M,R;
+    static int map[][];
+    static boolean visited[][];
+    static int dr[] = {0,1,0,-1};
+    static int dc[] = {1,0,-1,0};
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	}
-	
-	public static void print_arr() {
-		sb = new StringBuilder();
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				sb.append(arr[i][j] + " ");
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb.toString());
-	}
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
 
+        map =  new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int start;
+
+        for(int i=0;i<R;i++) {
+            visited = new boolean[N][M];
+            start = 0;
+            while (true) {
+                if (!check(start, start) || visited[start][start]) break;
+                rotate(start);
+                start++;
+            }
+        }
+        printMap();
+
+    }
+
+    private static void rotate(int start) {
+        int r = start;
+        int c = start;
+        int temp, nr, nc;
+
+        temp = map[r][c];
+
+        for(int dir=0;dir<4;dir++){
+            while(true){
+                nr = r + dr[dir];
+                nc = c + dc[dir];
+                if(!check(nr,nc) || visited[nr][nc]) break;
+
+                visited[r][c] = true;
+                map[r][c] = map[nr][nc];
+                r = nr;
+                c = nc;
+            }
+        }
+
+        map[start+1][start] = temp;
+
+    }
+
+    private static boolean check(int r, int c) {
+        return r>=0 && r<N && c>=0 && c<M;
+    }
+
+    private static void printMap() {
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                if(j!=M-1) System.out.print(map[i][j]+" ");
+                else  System.out.print(map[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
