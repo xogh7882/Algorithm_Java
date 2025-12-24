48a7a1efb50adec6a16d40b0b0d0b9617ce61bc1
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2025-12-24 19:22:28 +0900

    [Silver II] Title: 유기농 배추, Time: 100 ms, Memory: 13756 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -5,81 +5,73 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int T;
-	static int M,N,K;
-	static int[][] map;
-	static boolean[][] visited;
-	static int[] dr = {-1,0,1,0};
-	static int[] dc = {0,1,0,-1};
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(
-				new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		T = Integer.parseInt(st.nextToken());
-		
-		for(int test_case = 0 ; test_case < T ; test_case++ ) {
-			st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken());
-			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
-			map = new int[M][N];
-			for(int i=0;i<K;i++) {
-				st = new StringTokenizer(br.readLine());
-				int r = Integer.parseInt(st.nextToken());
-				int c = Integer.parseInt(st.nextToken());
-				map[r][c] = 1;
-			}
-			int count = 0;
-			visited = new boolean[M][N];
-			//mapPrint();
-			for(int i=0;i<M;i++) {
-				for(int j=0;j<N;j++) {
-					if(map[i][j] == 1 && visited[i][j] == false) {
-						count++;
-						BFS(i,j);
-					}
-				}
-			}
-			System.out.println(count);
-			
-		}
-	}
-	
-	
-	
-	private static void BFS(int r, int c) {
-		Queue<int[]> queue = new ArrayDeque<>();
-		queue.offer(new int[] {r,c});
-		while(!queue.isEmpty()) {
-			int temp[] = queue.poll();
-			for(int i=0;i<4;i++) {
-				int nr = temp[0] + dr[i];
-				int nc = temp[1] + dc[i];
-				if(check(nr,nc) && map[nr][nc] == 1 && visited[nr][nc] == false) {
-					visited[nr][nc] = true;
-					queue.offer(new int[] {nr,nc});
-				}
-			}
-		}
-	}
-	
-	
-	private static boolean check(int r, int c) {
-		return r>=0 && r<M && c>=0 && c<N;
-	}
+    static int T;
+    static int M,N,K;
+    static boolean[][] map;
+    static boolean[][] visited;
+    static int result;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
+        T = Integer.parseInt(br.readLine());
+        for(int t=1; t<=T; t++) {
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
 
-	private static void mapPrint() {
-		for(int i=0;i<M;i++) {
-			for(int j=0;j<N;j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		
-	}
+            map = new boolean[M][N];
+            visited = new boolean[M][N];
 
+            for(int num=0; num<K; num++){
+                st = new StringTokenizer(br.readLine());
+                int r = Integer.parseInt(st.nextToken());
+                int c = Integer.parseInt(st.nextToken());
+
+                map[r][c] = true;
+            }
+            result = 0;
+            for(int i=0;i<M;i++){
+                for(int j=0;j<N;j++){
+                    if(map[i][j] && !visited[i][j]){
+                        findResult(i,j);
+                        result++;
+                    }
+                }
+            }
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    private static void findResult(int r, int c) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{r,c});
+        visited[r][c] = true;
+        while(!queue.isEmpty()){
+            int[] cur = queue.poll();
+            int curR = cur[0];
+            int curC = cur[1];
+
+            for(int i=0;i<4;i++){
+                int nr = curR + dr[i];
+                int nc = curC + dc[i];
+
+                if(check(nr,nc) && !visited[nr][nc] && map[nr][nc]){
+                    visited[nr][nc] = true;
+                    queue.offer(new int[]{nr,nc});
+                }
+            }
+
+        }
+    }
+
+    private static boolean check(int r, int c){
+        return r>=0 && r<M && c>=0 && c<N;
+    }
 }
