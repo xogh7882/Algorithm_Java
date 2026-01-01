58309a720c8f2b82161d6eb759738d7a14ebc259
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2026-01-01 16:42:54 +0900

    [Silver III] Title: N과 M (7), Time: 544 ms, Memory: 218508 KB -BaekjoonHub

--- 백준/Silver/15656. N과 M （7）/N과 M （7）.java
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -1,58 +1,50 @@
-
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static int[] nums;
-	static int[] p;
-	static boolean[] visited;
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(
-				new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		nums = new int[M];
-		visited = new boolean[N];
-		p = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			p[i] = Integer.parseInt(st.nextToken());
-		}
-		Arrays.sort(p);
-		
-		StringBuilder sb = new StringBuilder();
-		perm(0,sb);
-		System.out.println(sb.toString());
-		
-		
-	}
+    static int N,M;
+    static int[] map;
+    static int[] select;
+    static StringBuilder sb = new StringBuilder();
 
-	private static void perm(int depth,  StringBuilder sb) {
-		if(depth == M) {
-			for(int i=0;i<M;i++) {
-				sb.append(nums[i] + " ");
-			}
-			sb.append("\n");
-			return;
-		}
-		for(int i=0;i<N;i++) {
-			//if(visited[i]) continue;
-			visited[i] = true;
-			nums[depth] = p[i];
-			perm(depth+1,sb);
-			nums[depth] = 0;
-			visited[i] = false;
-		}
-		
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N];
+        select = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            map[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(map);
+
+        findArr(0);
+
+        System.out.println(sb.toString());
+    }
+
+    private static void findArr(int cnt){
+        if(cnt == M){
+            for(int i=0;i<M;i++){
+                sb.append(select[i]);
+                if(i!=M-1) sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0;i<N;i++){
+            select[cnt] = map[i];
+            findArr(cnt+1);
+            select[cnt] = -1;
+        }
+    }
 }
