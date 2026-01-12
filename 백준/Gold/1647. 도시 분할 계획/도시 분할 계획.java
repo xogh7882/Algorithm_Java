9f2a3caab7c0604fdafeb8e77148ac44cfad154d
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2026-01-12 19:40:01 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1124 ms, Memory: 324740 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -4,89 +4,85 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M;
-	
-	static class Edge implements Comparable<Edge>{
-		int s;
-		int e;
-		int w;
-		public Edge(int s, int e, int w) {
-			super();
-			this.s = s;
-			this.e = e;
-			this.w = w;
-		}
-		@Override
-		public int compareTo(Edge o) {
-			return Integer.compare(this.w, o.w);
-		}
-	}
-	
-	static int p[];
-	static int r[];
-	static PriorityQueue<Edge> points;
-	static long min;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		points = new PriorityQueue<>();
-		
-		for(int i=0;i<M;i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			points.add(new Edge(s,e,w));
-		}
-		
-		makeset();
-		min = 0;
-		int cnt = 0;
-		
-		while(cnt != N -2) {
-			Edge edge = points.poll();
-			if(union(edge.s, edge.e)) {
-				cnt++;
-				min += edge.w;
-			}
-		}
-		System.out.println(min);
-	}
+    static int N,M;
+    static class Node implements Comparable<Node>{
+        int s;
+        int e;
+        int w;
 
-	private static boolean union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		
-		if(x==y) return false;
-		if(r[x] > r[y]) {
-			r[x] += r[y];
-			p[y] = x;
-		}
-		else {
-			r[y] += r[x];
-			p[x] = y;
-		}
-		return true;
-	}
+        public Node(int s, int e, int w){
+            this.s = s;
+            this.e = e;
+            this.w = w;
+        }
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.w, o.w);
+        }
+    }
 
-	private static int find(int x) {
-		if(x==p[x]) return p[x];
-		else return find(p[x]);
-	}
+    static PriorityQueue<Node> pq = new PriorityQueue<Node>();
+    static int r[];
+    static int p[];
+    static int min;
+    static int cnt = 0;
 
-	private static void makeset() {
-		p = new int[N+1];
-		r = new int[N+1];
-		
-		for(int i=0;i<N+1;i++) {
-			p[i] = i;
-			r[i] = 1;
-		}
-		
-	}
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            pq.offer(new Node(s,e,w));
+        }
+
+        // MST ( 근데 개수가 하나 더 작게 - 마을 나누기 )
+        // union & find
+
+        r = new int[N+1];
+        p = new int[N+1];
+        for(int i=0;i<N+1;i++){
+            r[i] = 1;  // rank
+            p[i] = i;  // parent
+        }
+
+        while(cnt != N-2){  // 1개 적게 연결
+            Node node = pq.poll();  // 간선 가중치 작은 것 하나 pq에서 꺼내기
+            if(union(node.s, node.e)){  // 만약 연결이 X
+                cnt++;          // 연결 간선 개수
+                min += node.w;  // 가중치 계산
+            }
+        }
+        System.out.println(min);
+    }
+
+    private static boolean union(int s, int e){
+        s = find(s);
+        e = find(e);
+        
+        if(s==e) return false; // 부모가 같다면 false
+        
+        if(r[s] > r[e]) {  // 더 개수가 많은 쪽으로 붙이기
+            r[s] += r[e];
+            p[e] = s;
+        }
+        else{
+            r[e] += r[s];
+            p[s] = e;
+        }
+        
+        return true;
+    }
+    
+    private static int find(int x){
+        if(x == p[x]) return p[x]; // 최종 부모 만나면 리턴
+        else return find(p[x]);  // 부모 찾을 때 까지 돌리기
+    }
 }
