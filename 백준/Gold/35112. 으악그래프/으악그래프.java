0e69f28f97d900bed330505d76a23801580605f5
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2026-01-18 22:50:21 +0900

    [Gold V] Title: 으악그래프, Time: 496 ms, Memory: 234088 KB -BaekjoonHub

---
+++ 백준/Gold/35112. 으악그래프/으악그래프.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+
+        }
+
+        // MST는 N-1 개의 간선으로 이루어짐 ( 1개 제거하면 갈라짐 )
+        // 2개 제거했을때 분리되야함
+        // 간선이 N개면 2개 제거하면 분리됨
+        // 더 많으면? 임의로 2개 지웠을 때, 안되는 경우 생김
+
+        if (M <= N) {
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+
+    }
+}
