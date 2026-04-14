3b938729fda3fe7047d33ebb8a54156ccbb31248
Taeho Lee <109503919+xogh7882@users.noreply.github.com>
2026-04-14 22:33:05 +0900

    [Gold IV] Title: 뱀, Time: 72 ms, Memory: 12244 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,108 +1,96 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main{
-	static int N,K,L;
-	static int map[][];
-	
-	static int dr[] = {0,1,0,-1};
-	static int dc[] = {1,0,-1,0};
-	
-	static int nowR = 1;
-	static int nowC = 1;
-	
-	static HashMap<Integer, String> time = new HashMap<Integer, String>();
-	
-	static Deque<int[]> location = new ArrayDeque<int[]>();
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		K = Integer.parseInt(br.readLine());
-		
-		map = new int[N+1][N+1];
-		
-		for(int i=0;i<K;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			map[a][b] = 9;
-		}
-		
-		L = Integer.parseInt(br.readLine());
-		
-		for(int i=0;i<L;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			String b = st.nextToken();
-			time.put(a, b);
-		}
-		
-		
-		map[nowR][nowC] = 1;
-		location.addLast(new int[] {nowR,nowC});
-		
-		int sec = 0;
-		int flag = 0;  // 뱀 머리 방향
-		
-		while(true) {
-						
-//			System.out.println("sec : " + sec);
-//			printmap();
-			
-			
-			if(time.containsKey(sec) == true) {
-				String ch = time.get(sec);
-				if(ch.equals("L")) flag--;
-				else flag++;
-				flag = (flag + 4) % 4;
-			}
-			
-			nowR += dr[flag];
-			nowC += dc[flag];
-			
-			if(check(nowR, nowC) == false) break;
-			
-			if(map[nowR][nowC] == 9) {
-				map[nowR][nowC] = 1;
-				location.addLast(new int[] {nowR,nowC});
-			}
-			else {
-				map[nowR][nowC] = 1;
-				location.addLast(new int[] {nowR,nowC});
-				int[] tail = location.pollFirst();
-				map[tail[0]][tail[1]] = 0;
-			}
-			
-			sec++;
-		}
-		
-		System.out.println(sec+1);
 
-	}
+public class Main {
+    static int N,K;
 
-	private static boolean check(int r, int c) {
-		return (r>=1 && r<=N && c>=1 && c<=N) && (map[r][c] != 1);
-	}
+    // 상 우 하 좌
+    static int dr[] = {-1,0,1,0};
+    static int dc[] = {0,1,0,-1};
+    public static void main(String[] args) throws Exception {
 
-	
-	private static void printmap() {
-		System.out.println();
-		System.out.println("---------Print Map----------");
-		
-		for(int i=1;i<=N;i++) {
-			for(int j=1;j<=N;j++) {
-				System.out.print(map[i][j] + " ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        K = Integer.parseInt(br.readLine());
+
+        int map[][] = new int[N+1][N+1];
+
+
+        for(int i=0;i<K;i++){
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            map[r][c] = 1;
+        }
+
+        int L = Integer.parseInt(br.readLine());
+        HashMap<Integer, Character> hashMap = new HashMap<>();
+        for(int i=0;i<L;i++){
+            st = new StringTokenizer(br.readLine());
+            int time = Integer.parseInt(st.nextToken());
+            char ch = st.nextToken().charAt(0);
+            hashMap.put(time, ch);
+        }
+
+        int result = 0;
+        int nowR = 1;
+        int nowC = 1;
+        int nowD = 1;
+        map[nowR][nowC] = 9;
+        LinkedList<int[]> list = new LinkedList<>();
+        list.add(new int[]{nowR, nowC});
+
+        while(true){
+            int nr = nowR + dr[nowD];
+            int nc = nowC + dc[nowD];
+
+            result++;
+
+            if(!check(nr, nc) || map[nr][nc] == 9) break;
+
+
+            // 사과 있으면 머리만 늘리고
+            if(map[nr][nc] == 1){
+                list.addFirst(new int[]{nr, nc});
+                map[nr][nc] = 9;
+            }
+
+            // 사과 없으면 머리 늘리고, 꼬리 줄이고
+            else{
+                list.addFirst(new int[]{nr, nc});
+                int[] tail = list.removeLast();
+                map[nr][nc] = 9;
+                map[tail[0]][tail[1]] = 0;
+            }
+
+            nowR = nr;
+            nowC = nc;
+
+            // 해당 시간에 머리 꺾어야하면
+            if(hashMap.containsKey(result)){
+                char ch = hashMap.get(result);
+                if(ch == 'L'){
+                    nowD--;
+                    if(nowD == -1) nowD = 3;
+                }
+                else {
+                    nowD++;
+                    if(nowD==4) nowD = 0;
+                }
+            }
+
+        }
+
+        System.out.println(result);
+
+    }
+
+    private static boolean check(int r, int c){
+        return r>=1 && r<=N && c>=1 && c<=N;
+    }
 
 }
